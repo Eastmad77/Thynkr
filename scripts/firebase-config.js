af6dd5574c9e3cb7ee5555a9d544bf534d32bf3b
Eastmad77 <brendan.hayman@gmail.com>
2025-10-17 20:38:22 +1300

    Create firebase-config.js

---
+++ scripts/firebase-config.js
@@ -0,0 +1,9 @@
+// /scripts/firebase-config.js
+export const firebaseConfig = {
+  apiKey: "AIzaSyDfjcMzAI-TIl0xsHri91VHiMdTGmd7b2k",
+  authDomain: "dailybrainbolt.firebaseapp.com",
+  projectId: "dailybrainbolt",
+  storageBucket: "dailybrainbolt.appspot.com",
+  messagingSenderId: "118224143962",
+  appId: "1:118224143962:web:replace_with_app_hash"
+};
