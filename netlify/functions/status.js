2c5fb73c5cabe164555ecc9e4b5fe62b6d8de345
Eastmad77 <brendan.hayman@gmail.com>
2025-10-20 12:21:36 +1300

    Update status.js

--- netlify/functions/status.js
+++ netlify/functions/status.js
@@ -1,16 +1,18 @@
+// netlify/functions/status.js
+const json = (statusCode, data, headers = {}) => ({
+  statusCode,
+  headers: { "content-type": "application/json", ...headers },
+  body: JSON.stringify(data),
+});
+
 export async function handler() {
-  const body = {
+  return json(200, {
     ok: true,
-    app: 'Whylee',
-    version: 'v7006',
-    time: new Date().toISOString()
-  };
-  return {
-    statusCode: 200,
-    headers: {
-      'Access-Control-Allow-Origin': '*',
-      'Content-Type': 'application/json; charset=utf-8'
+    env: {
+      NODE_ENV: process.env.NODE_ENV || "production",
+      STRIPE: !!process.env.STRIPE_SECRET_KEY,
+      FIREBASE_ADMIN: !!process.env.GOOGLE_APPLICATION_CREDENTIALS || !!process.env.FIREBASE_PRIVATE_KEY
     },
-    body: JSON.stringify(body)
-  };
+    version: "v9"
+  });
 }
