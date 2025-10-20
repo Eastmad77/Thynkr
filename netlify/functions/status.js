d054c4d2f1dce9ad9809d25c2493f6984c7b8250
Eastmad77 <brendan.hayman@gmail.com>
2025-10-20 14:30:05 +1300

    Update status.js

--- netlify/functions/status.js
+++ netlify/functions/status.js
@@ -1,9 +1,4 @@
-// netlify/functions/status.js
-const json = (statusCode, data, headers = {}) => ({
-  statusCode,
-  headers: { "content-type": "application/json", ...headers },
-  body: JSON.stringify(data),
-});
+const json = (s, d) => ({ statusCode: s, headers: { "content-type": "application/json" }, body: JSON.stringify(d) });
 
 export async function handler() {
   return json(200, {
@@ -11,7 +6,7 @@ export async function handler() {
     env: {
       NODE_ENV: process.env.NODE_ENV || "production",
       STRIPE: !!process.env.STRIPE_SECRET_KEY,
-      FIREBASE_ADMIN: !!process.env.GOOGLE_APPLICATION_CREDENTIALS || !!process.env.FIREBASE_PRIVATE_KEY
+      FIREBASE_ADMIN: !!process.env.FIREBASE_PROJECT_ID && !!process.env.FIREBASE_CLIENT_EMAIL && !!process.env.FIREBASE_PRIVATE_KEY
     },
     version: "v9"
   });
