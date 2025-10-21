5104e4b7292dadce8378c4e5f98fd6686269b84d
Eastmad77 <brendan.hayman@gmail.com>
2025-10-21 15:03:27 +1300

    Update firebase-config.js

--- scripts/firebase-config.js
+++ scripts/firebase-config.js
@@ -1,9 +1,13 @@
 // /scripts/firebase-config.js
+// Public Firebase Web SDK config for Whylee / DailyBrainBolt
+// Must load before any Firebase imports in HTML.
+
 export const firebaseConfig = {
-  apiKey: "AIzaSyDfjcMzAI-TIl0xsHri91VHiMdTGmd7b2k",
+  apiKey: "AIzaSyDfjcMzAl-Tll0xsHri91VHiMdTGmd7b2k",
   authDomain: "dailybrainbolt.firebaseapp.com",
   projectId: "dailybrainbolt",
-  storageBucket: "dailybrainbolt.appspot.com",
+  storageBucket: "dailybrainbolt.firebasestorage.app",
   messagingSenderId: "118224143962",
-  appId: "1:118224143962:web:replace_with_app_hash"
+  appId: "1:118224143962:web:43d85714b96ac1357e7a63",
+  measurementId: "G-M0P3TSCF8P"
 };
